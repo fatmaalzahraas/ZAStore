116f33695ceac0a448861da7695d0250d3c63701
fatmaalzahraas <fatma.alzahraa.syam@gmail.com>
2023-05-08 00:40:58 +0300

    build website

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('./src/assets/data/products.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: './build'
+});
+
 const port = 8000;
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1", 
+    })
+);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
