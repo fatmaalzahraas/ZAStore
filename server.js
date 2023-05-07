375e6551a84aef1ccf51b4b0f47ab94e4ca60953
fatmaalzahraas <fatma.alzahraa.syam@gmail.com>
2023-05-07 23:46:08 +0300

    zastore update

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const jsonServer = require("json-server");
+const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router('./src/assets/data/products.json');
 const middlewares = jsonServer.defaults();
+server.get("/favicon.ico", (req, res) => {
+res.status("200").sendFile(path(__dirname + "/favicon.ico")) 
+});
 const port = 8000;
 server.use(middlewares);
 server.use(router);
