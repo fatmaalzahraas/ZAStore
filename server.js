a44e4629cc9cab1e31a90a6d99d8f06297589197
fatmaalzahraas <fatma.alzahraa.syam@gmail.com>
2023-05-08 18:56:03 +0300

    commit

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-server.use(middlewares);
-server.use(router);
-server.listen(5000, () => {
-    console.log(`Json Server is running `);
-})
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/api') {
+    fs.readFile(path.join(__dirname, 'server', 'db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(data);
+    });
+  } else {
+    fs.readFile(path.join(__dirname, 'build', 'index.html'), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
