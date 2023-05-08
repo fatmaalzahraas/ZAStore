428b616b44623d59fa78c7284d96babcc19b2e2e
fatmaalzahraas <fatma.alzahraa.syam@gmail.com>
2023-05-08 18:44:55 +0300

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./src/assets/data/products.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
-server.listen(8000, () => {
+server.listen(5000, () => {
     console.log(`Json Server is running `);
 })
--- src/redux-toolkit/productsSlice.js
+++ src/redux-toolkit/productsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_URL = "http://localhost:8000/products";
+const API_URL = "http://localhost:5000/products";
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
   async (_, thunkAPI) => {
