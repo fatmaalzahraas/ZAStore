af748d71f47a88f019805beac41049732abec6fd
fatmaalzahraas <fatma.alzahraa.syam@gmail.com>
2023-05-08 17:19:27 +0300

    zastore update server.js

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('./src/assets/data/products.json');
-const middlewares = jsonServer.defaults({
-    static: './build'
-});
-
-const port = process.env.PORT || 8000;
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1", 
-    })
-);
 server.use(router);
-server.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+server.listen(8000, () => {
+    console.log(`Json Server is running `);
 })
